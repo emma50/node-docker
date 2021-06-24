55e2c8d8eb2b0d95c476136badc16514784e9202
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2021-06-24 16:00:33 +0100

    Clear server.js comments

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const database  = require( 'ronin-database' )
 const server = ronin.server()
 
 database.connect( process.env.CONNECTIONSTRING )
-// console.log(process.env)
-// console.log('12345678QWERTYUIWERTYUASDFGHZXCVB')
 
 server.use( '/foo', (req, res) => res.json({ "foo": "bar" }))
 server.use( '/', mocks.server( server.Router(), false, false ) )
